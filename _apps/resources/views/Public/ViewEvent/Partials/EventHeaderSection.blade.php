7dde80b71f47db5bd0f6c38fd4b11d6c65398f9c
Satheesh <satheesh@dreamstudio.in>
2018-12-07 16:52:53 +0530

    update

--- _apps/resources/views/Public/ViewEvent/Partials/EventHeaderSection.blade.php
+++ _apps/resources/views/Public/ViewEvent/Partials/EventHeaderSection.blade.php
@@ -51,7 +51,7 @@
                         <a class="btn btn-event-link btn-lg" href="{{{$event->event_url}}}#tickets">@lang("Public_ViewEvent.TICKETS")</a>
                     </div>
                     <div class="col-md-4 col-sm-4">
-                        <a class="btn btn-event-link btn-lg" href="{{{$event->event_url}}}#details">@lang("Public_ViewEvent.Details")</a>
+                        <a class="btn btn-event-link btn-lg" href="{{{$event->event_url}}}#details">@lang("Public_ViewEvent.DETAILS")</a>
                     </div>
                     <div class="col-md-4 col-sm-4">
                         <a class="btn btn-event-link btn-lg" href="{{{$event->event_url}}}#location">@lang("Public_ViewEvent.Description")</a>
