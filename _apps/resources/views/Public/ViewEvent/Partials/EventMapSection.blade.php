0a38081840406b1d9dc2e113a67e4a884fe06d96
Satheesh <satheesh@dreamstudio.in>
2018-12-07 17:26:26 +0530

    update

--- _apps/resources/views/Public/ViewEvent/Partials/EventMapSection.blade.php
+++ _apps/resources/views/Public/ViewEvent/Partials/EventMapSection.blade.php
@@ -1,18 +1,16 @@
 <section id="location" class="container">
         <div class="row">
-            <h1 class="section_head">
-                Event Details        </h1>
+            <h1 class="section_head">  Description  </h1>
         </div>
         <div class="row">
-                    <div class="col-md-12">
+            <div class="col-md-12">
                 <div class="content event_details" property="description">
                         <p>Players will be playing&nbsp;<strong>&quot;Classic - TPP - Solo&quot;</strong>&nbsp;in Custom Room on<strong>&nbsp;Asia Server.</strong></p>
                         <p>Check-in will start 15 min prior to the participant&#39;s round time.</p>
                         <p>After check-in you will be able to see&nbsp;<strong>Room ID and Password</strong>.</p>
                         <p>Go for the maximum points you can achieve as per mentioned in Game Rules.</p>
-     
     
-                    <h3>Important Points</h3>
+                        <h2>Important Points</h2>
     
                         <p>Get in Contact with Online Support Staff or Tournament Moderator(s) in case of any issues of Match Timing and Score reporting.</p>
                         <p>Check-IN is an essential part of participation and is compulsory for every participant.</p>
@@ -22,7 +20,7 @@
                     
                 </div>
             </div>
-                </div>
+      </div>
     </section>
 
 
