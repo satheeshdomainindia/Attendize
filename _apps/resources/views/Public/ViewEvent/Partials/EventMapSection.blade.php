720b10287bbaf6d2fbe2f00d04ea23d59dc7ba51
Satheesh <satheesh@dreamstudio.in>
2018-12-07 17:21:36 +0530

    update

--- _apps/resources/views/Public/ViewEvent/Partials/EventMapSection.blade.php
+++ _apps/resources/views/Public/ViewEvent/Partials/EventMapSection.blade.php
@@ -1,30 +1,23 @@
 <section id="location" class="container">
-        <div class="content event_details" property="description">
-        
-
-                <div class="row">
-                        <div class="col-md-12">
+    
+    <div class="row">
+        <div class="col-md-12">
+            <div class="content event_details" property="description">
                     <p>Players will be playing&nbsp;<strong>&quot;Classic - TPP - Solo&quot;</strong>&nbsp;in Custom Room on<strong>&nbsp;Asia Server.</strong></p>
                     <p>Check-in will start 15 min prior to the participant&#39;s round time.</p>
                     <p>After check-in you will be able to see&nbsp;<strong>Room ID and Password</strong>.</p>
                     <p>Go for the maximum points you can achieve as per mentioned in Game Rules.</p>
-                
-                        </div>
-                </div>
+ 
 
                 <h3>Important Points</h3>
-                <div class="row">
-                        <div class="col-md-12">
-               
+
                     <p>Get in Contact with Online Support Staff or Tournament Moderator(s) in case of any issues of Match Timing and Score reporting.</p>
                     <p>Check-IN is an essential part of participation and is compulsory for every participant.</p>
                     <p>No Rescheduling is Allowed in this Tournament.</p>
                     <p>Report the score with valid screenshots to claim your win.</p>
                     <p>Wait for your first match to show up. Regularly Check Brackets for Time Slots.</li>
                 
-                        </div>
-                </div>
-
-     
+             </div>
+        </div>
     </div>
 </section>
