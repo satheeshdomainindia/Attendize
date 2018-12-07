917a053632596a55cb27d4ff3a118d7e06199a8d
Satheesh <satheesh@dreamstudio.in>
2018-12-07 12:21:35 +0530

    update

--- _apps/config/attendize.php
+++ _apps/config/attendize.php
@@ -6,8 +6,8 @@ return [
 
     'ticket_status_sold_out'        => 1,
     'ticket_status_after_sale_date' => 2,//
-    'enable_test_payments'          => env('ENABLE_TEST_PAYMENTS', true),
-    'enable_dummy_payment_gateway'  => true,
+    'enable_test_payments'          => env('ENABLE_TEST_PAYMENTS', false),
+    'enable_dummy_payment_gateway'  => false,
     'payment_gateway_dummy'    => 0,
     'payment_gateway_stripe'   => 1,
     'payment_gateway_paypal'   => 2,
